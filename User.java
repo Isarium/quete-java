9658f941e0b4e9944f0dd4bbf5a60bfb5c9cc269
isarium <ludovic.bocquet@sncf.fr>
2020-11-10 15:11:22 +0100

    quest java

---
+++ User.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Date;
+//import Photo;
+//import Address;
+
+public class User {
+
+    Date birthDate;
+    int size;
+    ArrayList<Photo> photos;
+    Address address;
+}
